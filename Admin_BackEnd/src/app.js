44e20d8ab36b4bc205749ddf542e8537b46b3f12
Guru Pruthvi J M <Guru.Pruthvi@brillio.com>
2025-03-04 00:18:51 +0530

    final deployment

--- Admin_BackEnd/src/app.js
+++ Admin_BackEnd/src/app.js
@@ -12,10 +12,14 @@ async function createApp(){
     const app = express();
     app.use(express.json());
     const corsOptions = {
-        origin: 'http://54.166.126.188', // Adjust this if your frontend runs on a different URL or port
-        credentials: true,
+        origin: 'http://54.166.126.188', // Ensure this matches your frontend origin
+        credentials: true, // Allow credentials (cookies, authorization headers)
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed request methods
+        allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization' // Allowed headers
     };
-    app.use(cors());
+ 
+    app.use(cors(corsOptions));
+    app.options('*', cors(corsOptions));
     app.use(express.static(path.join(process.cwd(), 'public')))
     app.use(tokenDecorder(public_key, {algorithms: ['RS256']}));
     app.use('/api/employees', employeeRouter())
