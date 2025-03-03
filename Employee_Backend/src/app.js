eb7f061ab323e1e3d36f7832cda4826ee12e06fd
Guru Pruthvi J M <Guru.Pruthvi@brillio.com>
2025-03-04 00:05:43 +0530

    port change update #40

--- Employee_Backend/src/app.js
+++ Employee_Backend/src/app.js
@@ -10,7 +10,7 @@ async function createApp(){
     const app = express();
     app.use(express.json());
     const corsOptions = {
-        origin: 'http://54.166.126.188:7000', // Adjust this if your frontend runs on a different URL or port
+        origin: 'http://54.166.126.188', // Adjust this if your frontend runs on a different URL or port
         credentials: true,
     };
     app.use(cors(corsOptions));
