d9a690c3a2d56011064d30b48f295d697773d00e
Guru Pruthvi J M <Guru.Pruthvi@brillio.com>
2025-03-04 07:44:47 +0530

    allowing all the port to cors

--- Employee_Backend/src/app.js
+++ Employee_Backend/src/app.js
@@ -9,16 +9,28 @@ const cors = require('cors')
 async function createApp(){
     const app = express();
     app.use(express.json());
+    const allowedOrigins = [
+        'http://18.209.26.169',
+        'http://18.209.26.169:3000',
+        'http://18.209.26.169:7000',
+        'http://18.209.26.169:8000',
+        'http://18.209.26.169:8080',
+        'http://18.209.26.169:5000',
+    ];
     const corsOptions = {
-        origin: 'http://18.209.26.169', // Ensure this matches your frontend origin
-        credentials: true, // Allow credentials (cookies, authorization headers)
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed request methods
-        allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization' // Allowed headers
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, origin);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+        credentials: true, 
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization'
     };
- 
+
     app.use(cors(corsOptions));
- 
-    // Handle preflight (OPTIONS) requests globally
     app.options('*', cors(corsOptions));
     app.use(express.static(path.join(process.cwd(), 'public')))
     app.use(tokenDecorder(public_key, {algorithms: ['RS256']}));
