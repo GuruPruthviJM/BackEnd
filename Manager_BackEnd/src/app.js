50e308f4e363881dba2a945178b2997d518474f3
Guru Pruthvi J M <Guru.Pruthvi@brillio.com>
2025-03-04 10:36:06 +0530

    update cors #42

--- Manager_BackEnd/src/app.js
+++ Manager_BackEnd/src/app.js
@@ -13,29 +13,29 @@ async function createApp(){
 
     const app = express();
     app.use(express.json());
-    const allowedOrigins = [
-        'http://18.209.26.169',
-        'http://18.209.26.169:3000',
-        'http://18.209.26.169:7000',
-        'http://18.209.26.169:8000',
-        'http://18.209.26.169:8080',
-        'http://18.209.26.169:5000',
-    ];
-    const corsOptions = {
-        origin: function (origin, callback) {
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, origin);
-            } else {
-                callback(new Error('Not allowed by CORS'));
-            }
-        },
-        credentials: true, 
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization'
-    };
+    // const allowedOrigins = [
+    //     'http://18.209.26.169',
+    //     'http://18.209.26.169:3000',
+    //     'http://18.209.26.169:7000',
+    //     'http://18.209.26.169:8000',
+    //     'http://18.209.26.169:8080',
+    //     'http://18.209.26.169:5000',
+    // ];
+    // const corsOptions = {
+    //     origin: function (origin, callback) {
+    //         if (!origin || allowedOrigins.includes(origin)) {
+    //             callback(null, origin);
+    //         } else {
+    //             callback(new Error('Not allowed by CORS'));
+    //         }
+    //     },
+    //     credentials: true, 
+    //     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    //     allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization'
+    // };
 
-    app.use(cors(corsOptions));
-    app.options('*', cors(corsOptions));
+    app.use(cors());
+    // app.options('*', cors(corsOptions));
     app.use(express.static(path.join(process.cwd(), 'public')))
     app.use(tokenDecorder(public_key, {algorithms: ['RS256']}));
     app.use('/api/managers', managerRouter());
