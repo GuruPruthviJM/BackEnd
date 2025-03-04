6a684f56e89cee7a0b1c46ebe8a437da7d4067c6
Guru Pruthvi J M <Guru.Pruthvi@brillio.com>
2025-03-04 11:36:15 +0530

    modified code update #43

--- Admin_BackEnd/src/app.js
+++ Admin_BackEnd/src/app.js
@@ -5,10 +5,7 @@ const employeeRouter = require('./routers/employee.router');
 const ticketRouter = require('./routers/ticket.router');
 const managerRouter = require('./routers/manager.router');
 const adminRouter = require('./routers/admin.router');
-const authorizerRouter = require('./routers/authorizer.router');
-const customerRouter = require('./routers/customer.router');
-const emailRouter = require('./routers/email.router');
-const notificationRouter = require('./routers/notification.router');
+
 const { tokenDecorder } = require('ca-webutils/jwt');
 const cors = require('cors');
 
@@ -21,14 +18,17 @@ async function createApp() {
     const app = express();
     app.use(express.json());
 
+    // Define allowed origins for CORS
     // const allowedOrigins = [
     //     'http://18.209.26.169',
     //     'http://18.209.26.169:3000',
     //     'http://18.209.26.169:7000',
     //     'http://18.209.26.169:8000',
     //     'http://18.209.26.169:8080',
-    //     'http://18.209.26.169:5000',
+    //     'http://18.209.26.169:3000',
     // ];
+
+    // CORS Configuration
     // const corsOptions = {
     //     origin: function (origin, callback) {
     //         if (!origin || allowedOrigins.includes(origin)) {
@@ -43,6 +43,8 @@ async function createApp() {
     // };
 
     app.use(cors());
+
+    // Handle preflight (OPTIONS) requests globally
     // app.options('*', cors(corsOptions));
 
     app.use(express.static(path.join(process.cwd(), 'public')));
@@ -53,10 +55,10 @@ async function createApp() {
     app.use('/api/tickets', ticketRouter());
     app.use('/api/managers', managerRouter());
     app.use('/api/admins', adminRouter());
-    app.use('/api/authorizers', authorizerRouter());
-    app.use('/api/customers', customerRouter());
-    app.use('/api/email', emailRouter());
-    app.use('/api/notifications', notificationRouter());
+    // app.use('/api/authorizers', authorizerRouter());
+    // app.use('/api/customers', customerRouter());
+    // app.use('/api/email', emailRouter());
+    // app.use('/api/notifications', notificationRouter());
 
     return app;
 }
